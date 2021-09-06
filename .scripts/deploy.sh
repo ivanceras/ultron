6981e1c10d1b3891e6a8afe7cd13287f318e8c83
Jovansonlee Cesar <ivanceras@gmail.com>
2021-09-07 03:49:59 +0800

    Fix deploy script

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,9 +3,10 @@
 set -ev
 
 dest="../ivanceras.github.io/ultron/"
+src_dir="packages/ultron-web"
 
 . ./build.sh
 
 mkdir -p  "$dest"
-cp -r index.html favicon.ico pkg "$dest"
+cp -r $src_dir/index.html $src_dir/favicon.ico $src_dir/pkg $src_dir/assets "$dest"
 rm  $dest/pkg/.gitignore
