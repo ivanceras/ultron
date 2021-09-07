2e0086fcd2d7c70e33be31dca2d40c5a3da42f32
Jovansonlee Cesar <ivanceras@gmail.com>
2021-09-07 17:28:54 +0800

    Add publish script, start at 0.1.x

---
+++ .scripts/publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ev
+
+cd packages/syntaxes-themes && cargo publish && cd -  &&\
+
+echo "sleeping for 10s" && sleep 10 &&\
+
+cd  packages/ultron && cargo publish && cd - &&\
+
+echo "sleeping for 10s" && sleep 10 &&\
+
+cd packages/ultron-ssg && cargo publish && cd - &&\
+
+echo "sleeping for 10s" && sleep 10 &&\
+
+cd packages/ultron-web && cargo publish 
