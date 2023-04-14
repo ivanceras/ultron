aacb36e0be7e4822893c26006201f958227ad9bb
Jovansonlee Cesar <ivanceras@gmail.com>
2023-04-14 19:11:17 +0800

    refactor: simplify script, remove the outdated

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -v
-
-## must have these flag in order to enable copying of text into the browser clipboard
-## Take note also that the IP address it is served must not be 0.0.0.0
-RUSTFLAGS=--cfg=web_sys_unstable_apis wasm-pack build packages/ultron-app --target web --release --features "with-navigator-clipboard with-measure with-ric with-raf"
-
--- serve.sh
+++ serve.sh
@@ -3,6 +3,9 @@
 set -v
 
 
-. ./build.sh &&\
+RUSTFLAGS=--cfg=web_sys_unstable_apis wasm-pack build packages/ultron-app \
+    --target web \
+    --release \
+    --features "with-navigator-clipboard with-measure with-ric with-raf"
 
 basic-http-server  -a 127.0.0.1:4004 ./packages/ultron-app
