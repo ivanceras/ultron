533eb6aee68ec1572850956609538a5c83e5774a
Jovansonlee Cesar <ivanceras@gmail.com>
2023-06-20 10:30:52 +0800

    add deploy to alpha

---
+++ .scripts/deploy-alpha.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ev
+
+dest="../ivanceras.github.io/ultron.alpha/"
+src_dir="packages/ultron-app"
+
+just build-web
+
+mkdir -p  "$dest"
+cp -r $src_dir/index.html $src_dir/favicon.ico $src_dir/pkg $src_dir/fonts "$dest"
+rm  $dest/pkg/.gitignore
