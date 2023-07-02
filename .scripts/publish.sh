7a23e1b3d0c8c7f5488913d1323a654eb1b8002b
Jovansonlee Cesar <ivanceras@gmail.com>
2023-07-02 12:38:09 +0800

    fix script order

--- .scripts/publish.sh
+++ .scripts/publish.sh
@@ -2,10 +2,11 @@
 
 set -ev
 
-#cd packages/syntaxes-themes && cargo publish && cd -  &&\
+cd packages/syntaxes-themes && cargo publish && cd -  &&\
 
-#cd packages/ultron-core && RUSTFLAGS=--cfg=web_sys_unstable_apis cargo publish && cd - &&\
+cd packages/ultron-core && RUSTFLAGS=--cfg=web_sys_unstable_apis cargo publish && cd - &&\
 
-cd packages/ultron-ssg && cargo publish && cd - &&\
+cd packages/ultron-web && RUSTFLAGS=--cfg=web_sys_unstable_apis cargo publish  && cd - &&\
 
-cd packages/ultron-web && RUSTFLAGS=--cfg=web_sys_unstable_apis cargo publish
+cd packages/ultron-ssg && cargo publish
+
