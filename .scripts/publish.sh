04d3f63d7e9ff728f5d2c0528a4a881416ad7a6c
Jovansonlee Cesar <ivanceras@gmail.com>
2023-07-02 12:31:41 +0800

    fix ultron-web Cargo.toml

--- .scripts/publish.sh
+++ .scripts/publish.sh
@@ -4,7 +4,7 @@ set -ev
 
 #cd packages/syntaxes-themes && cargo publish && cd -  &&\
 
-cd packages/ultron-core && RUSTFLAGS=--cfg=web_sys_unstable_apis cargo publish && cd - &&\
+#cd packages/ultron-core && RUSTFLAGS=--cfg=web_sys_unstable_apis cargo publish && cd - &&\
 
 cd packages/ultron-ssg && cargo publish && cd - &&\
 
