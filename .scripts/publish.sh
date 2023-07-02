71817cbc7fec415d3f5469b19583e27a47b1cacb
Jovansonlee Cesar <ivanceras@gmail.com>
2023-07-02 12:29:20 +0800

    update dep and scripts for publishing

--- .scripts/publish.sh
+++ .scripts/publish.sh
@@ -2,16 +2,10 @@
 
 set -ev
 
-cd packages/syntaxes-themes && cargo publish && cd -  &&\
+#cd packages/syntaxes-themes && cargo publish && cd -  &&\
 
-echo "sleeping" && sleep 30 &&\
-
-cd  packages/ultron && RUSTFLAGS=--cfg=web_sys_unstable_apis cargo publish && cd - &&\
-
-echo "sleeping" && sleep 20 &&\
+cd packages/ultron-core && RUSTFLAGS=--cfg=web_sys_unstable_apis cargo publish && cd - &&\
 
 cd packages/ultron-ssg && cargo publish && cd - &&\
-
-echo "sleeping" && sleep 20 &&\
 
 cd packages/ultron-web && RUSTFLAGS=--cfg=web_sys_unstable_apis cargo publish
