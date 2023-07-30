cfb9c024f23084caa60ea7dfd303deb063f68432
Jovansonlee Cesar <ivanceras@gmail.com>
2023-07-31 06:04:15 +0800

    chore: remove some debugging

--- packages/ultron-web/src/font_loader.rs
+++ packages/ultron-web/src/font_loader.rs
@@ -64,7 +64,6 @@ impl<XMSG> Default for FontLoader<XMSG> {
 
 impl<XMSG> FontLoader<XMSG> {
     pub fn new(settings: &FontSettings) -> Self {
-        log::info!("Creating a new font loader with settings: {settings:?}");
         Self {
             settings: settings.clone(),
             ..Default::default()
@@ -87,32 +86,21 @@ impl<XMSG> FontLoader<XMSG> {
     }
 
     fn try_measure_font(&mut self) -> Vec<XMSG> {
-        log::debug!("Trying to measure font..");
         if self.is_fonts_loaded {
-            log::info!("try_measure_font, font_is_loaded: {}", self.is_fonts_loaded);
             if let Some((ch_width, ch_height)) = self.measure_font() {
                 self.ch_width = Some(ch_width);
                 self.ch_height = Some(ch_height);
-                log::info!(
-                    "font width: {:?}, height: {:?}",
-                    self.ch_width,
-                    self.ch_height
-                );
                 self.is_fonts_measured = true;
-                log::debug!("There are {} ready_listener", self.ready_listener.len());
                 self.ready_listener
                     .iter()
                     .map(|c| {
-                        log::trace!("emitting ready listener...");
                         c.emit(())
                     })
                     .collect()
             } else {
-                log::warn!("font measure element hasn't been mounted yet");
                 vec![]
             }
         } else {
-            log::warn!("try_measure_font: fonts hasn't been loaded yet");
             vec![]
         }
     }
@@ -127,7 +115,6 @@ where
     XMSG: 'static,
 {
     fn init(&mut self) -> Effects<Msg, XMSG> {
-        log::info!("Initializing font loader: {:?}", self.settings);
         let font_family = self.settings.font_family.to_owned();
         let font_src = self.settings.font_src.to_owned();
         let font_size = self.settings.font_size;
@@ -140,8 +127,6 @@ where
             JsFuture::from(font_set.load(&format!("{} {}", px(font_size), font_family)))
                 .await
                 .expect("font loaded");
-            log::info!("awaited the fonts loading...");
-            log::info!("triggering Msg::FontsLoaded: {font_family}");
             Msg::FontsLoaded
         }])
     }
@@ -149,18 +134,12 @@ where
     fn update(&mut self, msg: Msg) -> Effects<Msg, XMSG> {
         match msg {
             Msg::FontMeasureMounted(mount_event) => {
-                log::trace!("in Msg::FontMeasureMounted.");
-                let font_status = document().fonts().status();
-                log::info!("font status: {font_status:?}");
-                log::info!("font measure is mounted");
                 let elm: web_sys::Element = mount_event.target_node.unchecked_into();
                 self.mount_element = Some(elm);
-                log::info!("measure font: {:?}", self.measure_font());
                 let xmsgs = self.try_measure_font();
                 Effects::with_external(xmsgs)
             }
             Msg::FontsLoaded => {
-                log::info!("in Msg::FontsLoaded ");
                 self.is_fonts_loaded = true;
                 let xmsgs = self.try_measure_font();
                 Effects::with_external(xmsgs)
