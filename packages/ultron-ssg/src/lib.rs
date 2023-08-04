1b986aafef0f8df21fe17d1692cd87d792905103
Jovansonlee Cesar <ivanceras@gmail.com>
2023-08-05 06:35:13 +0800

    fix: make ultron-ssg independent from web_editor

--- packages/ultron-ssg/src/lib.rs
+++ packages/ultron-ssg/src/lib.rs
@@ -1,9 +1,123 @@
 #![deny(warnings)]
+use css_colors::rgba;
+use css_colors::Color as ColorTrait;
+use css_colors::RGBA;
+use sauron::*;
+use ultron_syntaxes_themes::Color;
+use ultron_syntaxes_themes::Style;
+use ultron_syntaxes_themes::TextHighlighter;
+use ultron_syntaxes_themes::Theme;
 
-use ultron_web::{
-    sauron::{html::attributes::r#type, html::tags::style, html::text, *},
-    Options, WebEditor,
-};
+const COMPONENT_NAME: &str = "ssg";
+
+#[derive(Debug, Default)]
+struct Options {
+    show_line_numbers: bool,
+}
+
+struct CodeViewer {
+    options: Options,
+    lines: Vec<String>,
+    active_theme: &'static Theme,
+}
+
+impl CodeViewer {
+    fn view<MSG>(&self, text_highlighter: &mut TextHighlighter) -> Node<MSG> {
+        let class_ns = |class_names| class_namespaced(COMPONENT_NAME, class_names);
+        let code_attributes = [
+            class_ns("code"),
+            style! {
+                background: self.theme_background().to_css(),
+                font_family: "monospace",
+            },
+        ];
+
+        let rendered_lines = self.lines.iter().enumerate().map(|(line_index, line)| {
+            let hl_line = text_highlighter
+                .highlight_line(&line)
+                .expect("must highlight");
+            div([class_ns("line")], {
+                [self.view_line_number(line_index + 1)]
+                    .into_iter()
+                    .chain(self.view_highlighted_line(&hl_line))
+                    .collect::<Vec<_>>()
+            })
+        });
+
+        div(code_attributes, rendered_lines)
+    }
+
+    fn view_highlighted_line<MSG>(&self, line: &[(Style, &str)]) -> Vec<Node<MSG>> {
+        line.iter()
+            .map(|(style, range)| {
+                let foreground = to_rgba(style.foreground).to_css();
+                span([style! { color: foreground }], [text(range)])
+            })
+            .collect()
+    }
+
+    fn view_line_number<MSG>(&self, line_number: usize) -> Node<MSG> {
+        let class_ns = |class_names| class_namespaced(COMPONENT_NAME, class_names);
+        view_if(
+            self.options.show_line_numbers,
+            span(
+                [
+                    class_ns("number"),
+                    style! {
+                        user_select: "none",
+                        "-webkit-user-select": "none",
+                        display: "inline-block",
+                        background_color: self.gutter_background().to_css(),
+                        color: self.gutter_foreground().to_css(),
+                        width: ch(self.numberline_wide()),
+                        padding_right: ch(self.numberline_padding_wide()),
+                    },
+                ],
+                [text(line_number)],
+            ),
+        )
+    }
+
+    fn numberline_wide(&self) -> usize {
+        self.lines.len().to_string().len()
+    }
+
+    fn theme_background(&self) -> RGBA {
+        let default = rgba(255, 255, 255, 1.0);
+        self.active_theme
+            .settings
+            .background
+            .map(to_rgba)
+            .unwrap_or(default)
+    }
+
+    fn gutter_background(&self) -> RGBA {
+        let default = rgba(0, 0, 0, 1.0);
+        self.active_theme
+            .settings
+            .gutter
+            .map(to_rgba)
+            .unwrap_or(default)
+    }
+
+    fn gutter_foreground(&self) -> RGBA {
+        let default = rgba(0, 0, 0, 1.0);
+        self.active_theme
+            .settings
+            .gutter_foreground
+            .map(to_rgba)
+            .unwrap_or(default)
+    }
+
+    pub(crate) fn numberline_padding_wide(&self) -> usize {
+        1
+    }
+}
+
+pub fn to_rgba(color: Color) -> RGBA {
+    let Color { r, g, b, a } = color;
+    css_colors::rgba(r, g, b, a as f32 / 255.0)
+}
 
 pub fn render<MSG>(content: &str, syntax_token: &str, theme_name: Option<&str>) -> Node<MSG>
 where
@@ -11,42 +125,23 @@ where
 {
     let options = Options {
         show_line_numbers: true,
-        show_status_line: false,
-        show_cursor: false,
-        use_spans: true,
-        use_for_ssg: true,
-        //Important that this is set,
-        //since it is needed in rendering the line with
-        //a fixed height
-        ch_height: Some(16.),
-        theme_name: theme_name.map(|s| s.to_string()),
-        syntax_token: syntax_token.to_string(),
-        ..Default::default()
     };
-    let web_editor = WebEditor::from_str(&options, content);
-    page(web_editor)
+
+    let mut text_highlighter = TextHighlighter::default();
+    if let Some(theme_name) = theme_name {
+        text_highlighter.select_theme(theme_name);
+    }
+    text_highlighter.set_syntax_token(syntax_token);
+
+    let viewer = CodeViewer {
+        options,
+        lines: content.lines().map(|s| s.to_string()).collect(),
+        active_theme: text_highlighter.active_theme(),
+    };
+    viewer.view(&mut text_highlighter)
 }
 
 pub fn render_to_string(content: &str, syntax_token: &str, theme_name: Option<&str>) -> String {
     let node = render::<()>(content, syntax_token, theme_name);
     node.render_to_string()
-}
-
-fn page<MSG>(web_editor: WebEditor<MSG>) -> Node<MSG>
-where
-    MSG: 'static,
-{
-    main(
-        [],
-        [
-            header(
-                [],
-                [style(
-                    [r#type("text/css")],
-                    [text(web_editor.style().join(""))],
-                )],
-            ),
-            article([], [web_editor.plain_view()]),
-        ],
-    )
 }
